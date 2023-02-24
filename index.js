09550430b956362c19d51ccbf1c89d6557f8e1c0
SOME1HING <yashprakash2005@gmail.com>
2023-02-25 01:03:01 +0530

    ```INITIAL COMMIT```

---
+++ index.js
@@ -0,0 +1,47 @@
+// ----------------------Imports-----------------------------
+import { Snake } from "tgsnake";
+import * as dotenv from "dotenv";
+
+// ----------------------Init-----------------------------
+dotenv.config();
+
+const bot = new Snake({
+  apiId: process.env.API,
+  apiHash: process.env.HASH,
+});
+
+try {
+  bot.run();
+} catch (err) {
+  console.log(
+    "Error: clear the file cache.json and paste [] – and check your code for errors."
+  );
+}
+
+// ----------------------Commands-----------------------------
+bot.command("start", async (ctx) => {
+  await ctx.reply("Bot started");
+});
+
+bot.command("banall", async (ctx) => {
+  let success = 0;
+  let fail = 0;
+
+  const results = await ctx.telegram.getParticipants(ctx.chat.id);
+
+  for (const i in results["participants"]) {
+    try {
+      await ctx.telegram.banChatMember(
+        ctx.chat.id,
+        results["participants"][i]["user"]["id"]
+      );
+      success++;
+    } catch (error) {
+      fail++;
+      console.log(error);
+    }
+  }
+  await ctx.reply(`Successfully Banned: ${success} users\nFailed: ${fail}`);
+});
+
+// ---------------------------------------------------
